e0bcf0030e7e546f5240468086e4543714d4729b
DimitriF <dimitrifichou@laposte.net>
2015-11-19 00:18:25 +0100

    renderd3tf removal

--- server.R
+++ server.R
@@ -43,24 +43,6 @@ shinyServer(function(input, output,session) {
 #   session$onSessionEnded(function() {
 #     obs$suspend()
 #   })
-  output$mtcars <- renderD3tf({
-    
-    # Define table properties. See http://tablefilter.free.fr/doc.php
-    # for a complete reference
-    tableProps <- list(
-      btn_reset = TRUE,
-      sort = TRUE,
-      sort_config = list(
-        # alphabetic sorting for the row names column, numeric for all other columns
-        sort_types = c("String", rep("Number", ncol(mtcars)))
-      )
-    );
-    
-    d3tf(mtcars,
-         tableProps = tableProps,
-         showRowNames = TRUE,
-         tableStyle = "table table-bordered");
-  })
   #### demo batch #####
   output$download.demo.batch <- downloadHandler(
     filename = "rTLC_demobatch.xls",
