f542e30be488a5ec639acdf0d7a82a7bbc4544a2
DimitriF <dimitrifichou@laposte.net>
2015-11-28 17:30:10 +0100

    google analytics

--- ui.R
+++ ui.R
@@ -680,6 +680,7 @@ print(ggplot(data,aes(x=box,fill=Var.Dep))+geom_bar())
                               )
                             )
                             ),
+                   tags$head(includeScript("google-analytics.js")),
                    tags$head(tags$style(type="text/css", "tfoot {display: table-header-group}")),
                    tags$head(tags$style(HTML(".shiny-output-error-validation {color: red;font-size: 24px}"))),
                    tags$head(tags$style(type="text/css", ".shiny-progress .progress {position: absolute;width: 100%;top: 100px;height: 10px;margin: 0px;}")),
